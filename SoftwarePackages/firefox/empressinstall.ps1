27df1781e8ebbd144f4004238f232fe8d54f24d5
Neocky <Neocky@users.noreply.github.com>
2021-02-04 16:49:03 +0100

    Added package: firefox
    Added firefox as package.

---
+++ SoftwarePackages/firefox/empressinstall.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+Downloads and executes the firefox installer.
+
+.DESCRIPTION
+This scirpt will download and install the latest firefox version
+#>
+$empressFolder = "C:\Empress\firefox"
+$downloadlink = "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US"
+$argumentList = "/s"
+
+function installPackage {
+    [CmdletBinding()]
+    param (
+        [Parameter()][String]$setupFolder,
+        [Parameter()][String]$url,
+        [Parameter()][String]$arguments
+    )
+    $installerPath = $setupFolder + "\setup.exe"
+    $webBrowser = New-Object System.Net.WebClient
+    Try {
+        $webBrowser.DownloadFile($url, $installerPath)
+    } catch {
+        Exit 1
+    }
+    Try {
+        Start-Process -FilePath $installerPath -ArgumentList $arguments -Verb RunAs -Wait
+    } catch {
+        Exit 1
+    }
+}
+
+installPackage -setupFolder $empressFolder -url $downloadlink -arguments $argumentList
