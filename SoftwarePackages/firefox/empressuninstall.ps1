21cc820ff6e21444332d38c264dad8f186878ea7
Neocky <Neocky@users.noreply.github.com>
2021-02-11 09:00:22 +0100

    Created empressuninstall.ps1
    Created uninstall.ps1 script for future package deletion feature.

---
+++ SoftwarePackages/firefox/empressuninstall.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.SYNOPSIS
+Executes the firefox uninstaller.exe.
+
+.DESCRIPTION
+This scirpt will uninstall the installed firefox version.
+#>
+$empressFolder = "C:\Empress\firefox"
+$uninstallerExe = "\uninstall\helper.exe"
+$argumentList = "/s"
+
+function uninstallPackage {
+    [CmdletBinding()]
+    param (
+        [Parameter()][String]$setupFolder,
+        [Parameter()][String]$uninstaller,
+        [Parameter()][String]$arguments
+    )
+    $uninstallerPath = $setupFolder + $uninstaller
+    Try {
+        Start-Process -FilePath $uninstallerPath -ArgumentList $arguments -Verb RunAs -Wait
+    } catch {
+        Exit
+    }
+}
+
+uninstallPackage -setupFolder $empressFolder -uninstaller $uninstallerExe  -arguments $argumentList
