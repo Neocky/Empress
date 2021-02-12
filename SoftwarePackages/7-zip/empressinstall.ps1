fb841753ec10fda2b27ff2aa2e97347a66ad46cf
Neocky <Neocky@users.noreply.github.com>
2021-02-12 16:40:41 +0100

    Added 7-zip as software package

---
+++ SoftwarePackages/7-zip/empressinstall.ps1
@@ -0,0 +1,34 @@
+﻿<#
+.SYNOPSIS
+Downloads and executes the 7-zip installer.
+
+.DESCRIPTION
+This scirpt will download and install the latest stable 7-zip version
+
+#>
+$empressFolder = "C:\Empress\7-zip"
+$downloadlink = "https://7-zip.org/a/7z1900-x64.exe"
+$argumentList = "/S /D=$empressFolder"
+
+function installPackage {
+    [CmdletBinding()]
+    param (
+        [Parameter()][String]$setupFolder,
+        [Parameter()][String]$url,
+        [Parameter()][String]$arguments
+    )
+    $installerPath = $setupFolder + "\empresssetup.exe"
+    $webBrowser = New-Object System.Net.WebClient
+    Try {
+        $webBrowser.DownloadFile($url, $installerPath)
+    } catch {
+        Exit 1
+    }
+    Try {
+        Start-Process -FilePath $installerPath -ArgumentList $arguments -Verb RunAs -Wait
+    } catch {
+        Exit 1
+    }
+}
+
+installPackage -setupFolder $empressFolder -url $downloadlink -arguments $argumentList
---
+++ SoftwarePackages/7-zip/empressuninstall.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+Executes the 7-zip uninstaller.exe.
+
+.DESCRIPTION
+This scirpt will uninstall the installed 7-zip version.
+
+#>
+$empressFolder = "C:\Empress\7-zip"
+$uninstallerExe = "\Uninstall.exe"
+$argumentList = "/S"
+
+function uninstallPackage {
+    [CmdletBinding()]
+    param (
+        [Parameter()][String]$setupFolder,
+        [Parameter()][String]$uninstaller,
+        [Parameter()][String]$arguments
+    )
+    $uninstallerPath = $setupFolder + $uninstaller
+    Try {
+        Start-Process -FilePath $uninstallerPath -ArgumentList $arguments -Verb RunAs -Wait
+    } catch {
+        Exit
+    }
+}
+
+uninstallPackage -setupFolder $empressFolder -uninstaller $uninstallerExe  -arguments $argumentList
