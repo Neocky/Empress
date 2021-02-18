bd6830f6a2c1b11d028a8ec9422a40e5e60a27aa
Neocky <Neocky@users.noreply.github.com>
2021-02-18 16:00:15 +0100

    Added package lenovosystemupdate
    Added the lenovo system update package.

---
+++ SoftwarePackages/lenovosystemupdate/empressinstall.ps1
@@ -0,0 +1,33 @@
+﻿<#
+.SYNOPSIS
+Downloads and executes the lenovosystemupdate installer.
+
+.DESCRIPTION
+This scirpt will download and install the latest lenovosystemupdate version
+#>
+$empressFolder = "C:\Empress\lenovosystemupdate"
+$downloadlink = "https://download.lenovo.com/pccbbs/thinkvantage_en/system_update_5.07.0117.exe"
+$argumentList = "/VERYSILENT /NORESTART /DIR=$empressFolder"
+
+function installPackage {
+    [CmdletBinding()]
+    param (
+        [Parameter()][String]$setupFolder,
+        [Parameter()][String]$url,
+        [Parameter()][String]$arguments
+    )
+    $installerPath = $setupFolder + "\empresssetup.exe"
+    $webBrowser = New-Object System.Net.WebClient
+    Try {
+        $webBrowser.DownloadFile($url, $installerPath)
+    } catch {
+        Exit 1
+    }
+    Try {
+        Start-Process -FilePath $installerPath -ArgumentList $arguments -Verb RunAs -Wait
+    } catch {
+        Exit 1
+    }
+}
+
+installPackage -setupFolder $empressFolder -url $downloadlink -arguments $argumentList
---
+++ SoftwarePackages/lenovosystemupdate/empressuninstall.ps1
@@ -0,0 +1,27 @@
+﻿<#
+.SYNOPSIS
+Executes the lenovosystemupdate uninstaller.exe.
+
+.DESCRIPTION
+This scirpt will uninstall the installed lenovosystemupdate version.
+#>
+$empressFolder = "C:\Empress\lenovosystemupdate"
+$uninstallerExe = "\unins000.exe.exe"
+$argumentList = "/s"
+
+function uninstallPackage {
+    [CmdletBinding()]
+    param (
+        [Parameter()][String]$setupFolder,
+        [Parameter()][String]$uninstaller,
+        [Parameter()][String]$arguments
+    )
+    $uninstallerPath = $setupFolder + $uninstaller
+    Try {
+        Start-Process -FilePath $uninstallerPath -ArgumentList $arguments -Verb RunAs -Wait
+    } catch {
+        Exit
+    }
+}
+
+uninstallPackage -setupFolder $empressFolder -uninstaller $uninstallerExe  -arguments $argumentList
